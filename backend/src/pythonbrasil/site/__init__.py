386e0accaa3a8cea5224530f8abf570d05efaa6b
Érico Andrei <ericof@gmail.com>
2025-09-09 16:14:34 +0200

    Update techevent to version 1.0.0a19

--- backend/src/pythonbrasil/site/__init__.py
+++ backend/src/pythonbrasil/site/__init__.py
@@ -12,3 +12,12 @@ PACKAGE_NAME = "pythonbrasil.site"
 _ = MessageFactory(PACKAGE_NAME)
 
 logger = logging.getLogger(PACKAGE_NAME)
+
+
+def run_patches():
+    from pythonbrasil.site.patch import apply_patches
+
+    apply_patches()
+
+
+run_patches()
---
+++ frontend/packages/volto-pythonbrasil-site/news/+techevent.feature
@@ -0,0 +1,1 @@
+Upgrade @plone-collective/volto-techevent to 1.0.0-alpha.19. @ericof
